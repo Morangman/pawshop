5884e22a854cbdcfc598019cd73e234c41a9e04a
d.sinepolsky <d.sinepolsky@amgrade.com>
2021-02-28 23:42:35 +0200

    Style fixes

--- resources/js/components/Account.vue
+++ resources/js/components/Account.vue
@@ -113,18 +113,13 @@
                                                                         <input v-model="model.addresses[index].postal_code" name="postal-code" autocomplete="on" type="text" placeholder="Postal Code*">
                                                                     </div>
                                                                     <div class="input-block">
-                                                                        <div class="row">
-                                                                            <div class="checkbox-absolute custom-control custom-checkbox">
-                                                                                <input type="checkbox" :checked="model.addresses[index].is_default === '1'" name="is_default">
-                                                                            </div>
-                                                                            <b-form-checkbox
-                                                                                v-model="model.addresses[index].is_default"
-                                                                                value="1"
-                                                                                unchecked-value="0"
-                                                                            >
-                                                                                Default Address
-                                                                            </b-form-checkbox>
-                                                                        </div>
+                                                                        <b-form-checkbox
+                                                                            v-model="model.addresses[index].is_default"
+                                                                            value="1"
+                                                                            unchecked-value="0"
+                                                                        >
+                                                                            Default Address
+                                                                        </b-form-checkbox>
                                                                     </div>
                                                                 </form>
                                                             </div>
