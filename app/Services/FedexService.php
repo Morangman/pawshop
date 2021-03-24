ab883d98261801fcbe4c56a9f11cf445c3f67f88
Dmitry <sinepolsky.dmitry@gmail.com>
2021-03-24 13:55:49 +0200

    Show FedEx error

--- app/Services/FedexService.php
+++ app/Services/FedexService.php
@@ -183,7 +183,7 @@ class FedexService
 
         //Storage::put('public/pdf/invoice.pdf', $result->CompletedShipmentDetail->CompletedPackageDetails[0]->Label->Parts[0]->Image);
 
-        //Storage::put('public/pdf/info.json', json_encode($result->toArray(), JSON_PRETTY_PRINT));
+        Storage::put('public/pdf/info.json', json_encode($result->toArray(), JSON_PRETTY_PRINT));
 
         if ($arrayResult['HighestSeverity'] === 'SUCCESS' || $arrayResult['HighestSeverity'] === 'NOTE') {
             //var_dump($result->CompletedShipmentDetail->CompletedPackageDetails[0]->Label->Parts[0]->Image);
