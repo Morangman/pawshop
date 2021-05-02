98e3bbcf7c32185bd8032e8739a717a81f4f9e13
Dmitry <sinepolsky.dmitry@gmail.com>
2021-05-02 16:55:26 +0300

    Fix email title

--- resources/views/mail/account_verification.blade.php
+++ resources/views/mail/account_verification.blade.php
@@ -87,7 +87,7 @@
               <tbody>
                 <tr>
                   <td class="o_bg-dark o_px o_py-md o_br-t o_sans o_text" align="center" style="font-family: Helvetica, Arial, sans-serif;margin-top: 0px;margin-bottom: 0px;font-size: 16px;line-height: 24px;background-color: #242b3d;border-radius: 4px 4px 0px 0px;padding-left: 16px;padding-right: 16px;padding-top: 24px;padding-bottom: 24px;">
-                    <p style="margin-top: 0px;margin-bottom: 0px;"><a class="o_text-white" href="{{ URL::route('home') }}" style="text-decoration: none;outline: none;color: #ffffff;"><img src="{{ URL::asset('client/images/white-main-logo.png') }}" width="136" height="27" alt="RapidRecycle" style="max-width: 136px;-ms-interpolation-mode: bicubic;vertical-align: middle;border: 0;line-height: 100%;height: auto;outline: none;text-decoration: none;"></a></p>
+                    <p style="margin-top: 0px;margin-bottom: 0px;"><a class="o_text-white" href="{{ URL::route('home') }}" style="text-decoration: none;outline: none;color: #ffffff;"><img src="{{ URL::asset('client/images/white-main-logo.png') }}" width="136" height="27" alt="Rapid Recycle" style="max-width: 136px;-ms-interpolation-mode: bicubic;vertical-align: middle;border: 0;line-height: 100%;height: auto;outline: none;text-decoration: none;"></a></p>
                   </td>
                 </tr>
               </tbody>
