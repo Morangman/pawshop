20864bc1da0b2531f40cf1faa3415409169a3ecc
Dmitry <sinepolsky.dmitry@gmail.com>
2021-05-07 20:59:36 +0300

    Product edit changes

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -152,58 +152,72 @@ class ProductController extends Controller
 
         $priceVariations = [];
 
-        // $data = DB::table('prices')->where('category_id', $category->getKey())->get()
-        //     ->each(function ($price) use ($priceVariations) {
-        //         $ids = json_decode($price->steps_ids);
+        if (!$category->getAttribute('is_parsed')) {
+            $prices = DB::table('prices')->where('category_id', $category->getKey())->get();
 
-        //         $steps = Step::query()->whereIn('id', $ids)->get()->toArray();
-
-        //         $priceVariations[] = [
-        //             'steps' => $steps,
-        //             'price' => $price->price,
-        //             'custom_price' => $price->custom_price,
-        //             'id' => $price->id,
-        //         ];
-        //     });
-
-        // dd($priceVariations);
-
-        $pricesByCategory = DB::table('prices')->where('category_id', $category->getKey())->get()->toArray();
-
-        $idsArray = [];
-
-        foreach ($pricesByCategory as $index => $obj) {
-            $idsArray[] = json_decode($obj->steps_ids);
-        }
-
-        $idsArray = array_unique($idsArray, SORT_REGULAR);
-
-        foreach ($idsArray as $ids) {
-            $price = DB::table('prices')->where('category_id', $category->getKey())->whereJsonContains('steps_ids', $ids)->first();
-            $steps = Step::query()->whereIn('id', $ids)->get();
-            $stepsArray = $steps->toArray();
-            $isBroken = false;
-
-            foreach ($steps as $i => $step) {
-                $stepArray = $step->toArray();
-
-                if ($category->getAttribute('is_parsed')) {
-                    $stepArray['value'] === 'Brand New' ? $stepsArray[$i]['value'] = 'Flawless' : $stepArray['value'];
+            foreach($prices as $price) {
+                $isBroken = false;
+                
+                $ids = json_decode($price->steps_ids);
+    
+                $steps = Step::query()->whereIn('id', $ids)->with('stepName')->get()->toArray();
+    
+                foreach ($steps as $key => $step) {
+                    if ($category->getAttribute('is_parsed')) {
+                        $step['value'] === 'Brand New' ? $steps[$key]['value'] = 'Flawless' : $step['value'];
+                    }
+    
+                    if (isset($step['step_name']['is_functional']) && $step['value'] === 'No') {
+                        $isBroken = true;
+                    }
                 }
-
-                if ($step->stepName->is_functional && $stepArray['value'] === 'No') {
-                    $isBroken = true;
+    
+                if (!$isBroken) {
+                    $priceVariations[] = [
+                        'steps' => $steps,
+                        'price' => $price->price,
+                        'custom_price' => $price->custom_price,
+                        'id' => $price->id,
+                    ];
                 }
             }
-
-            if (!$isBroken) {
-                $priceVariations[] = [
-                'steps' => $steps,
-                'price' => $price->price,
-                'custom_price' => $price->custom_price,
-                'id' => $price->id,
-            ];
         }
+
+        if ($category->getAttribute('is_parsed')) {
+            $pricesByCategory = DB::table('prices')->where('category_id', $category->getKey())->get()->toArray();
+
+            $idsArray = [];
+    
+            foreach ($pricesByCategory as $index => $obj) {
+                $idsArray[] = json_decode($obj->steps_ids);
+            }
+    
+            $idsArray = array_unique($idsArray, SORT_REGULAR);
+    
+            foreach ($idsArray as $ids) {
+                $price = DB::table('prices')->where('category_id', $category->getKey())->whereJsonContains('steps_ids', $ids)->first();
+                $steps = Step::query()->whereIn('id', $ids)->with('stepName')->get()->toArray();
+                $isBroken = false;
+    
+                foreach ($steps as $i => $step) {
+                    if ($category->getAttribute('is_parsed')) {
+                        $step['value'] === 'Brand New' ? $steps[$i]['value'] = 'Flawless' : $step['value'];
+                    }
+    
+                    if (isset($step['step_name']['is_functional']) && $step['value'] === 'No') {
+                        $isBroken = true;
+                    }
+                }
+    
+                if (!$isBroken) {
+                    $priceVariations[] = [
+                        'steps' => $steps,
+                        'price' => $price->price,
+                        'custom_price' => $price->custom_price,
+                        'id' => $price->id,
+                    ];
+                }
+            }
         }
 
         if ($steps = $category->steps()->get()) {
