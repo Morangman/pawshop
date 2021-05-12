e55fbfa73d493d624dfdc2d335129b2d04d708ad
Dmitry <sinepolsky.dmitry@gmail.com>
2021-05-12 15:36:36 +0300

    Changes in chat email

--- resources/views/mail/message.blade.php
+++ resources/views/mail/message.blade.php
@@ -7,7 +7,8 @@
     <style type="text/css">
         .quote {
             border-left: 2px solid #000;
-            padding-left: 15px
+            padding-left: 15px;
+            margin-top: 20px;
         }
 
         #signature {
@@ -16,18 +17,14 @@
     </style>
 </head>
 <body>
+    <p>{{ $data['text'] }}</p>
+
     @if(isset($data['last_message']))
         <div class="quote">{{ $data['last_message'] }}</div>
     @endif
 
-    <p>{{ $data['text'] }}</p>
-
     <div id="signature">
-        <font style="font-family:'Century Gothic', Arial, sans-serif; font-size: 9pt; color:#333;line-height:0.5;">
-            <p id="name" style="color: #126de5; font-size:120%; font-weight:bolder; line-height: 0.5;">Rapid Recycle</p>
-            <p id="position">Rapid Recycle Team</p>
-            <p id="tel">1730 E Warner Rd, Suite 7, Tempe, AZ 85284, USA</p>
-        </font>
+        <p>The Rapid Recycle Team</p>
     </div>
 </body>
 </html>
