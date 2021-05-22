e0009144f283ece35a98e6c47b2ef782e2d4288c
Dmitry <sinepolsky.dmitry@gmail.com>
2021-05-22 22:16:50 +0300

    Front-end changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,17 +22,6 @@
     <link href="{{ asset('client/css/app.css') }}" rel="stylesheet" type="text/css">
 
     {!! $settings->getAttribute('code_insert') !!}
-
-    <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.sitekey') }}"></script>
-    <script>
-        grecaptcha.ready(function() {
-            grecaptcha.execute('{{ config('services.recaptcha.sitekey') }}', {action: 'contact'}).then(function(token) {
-            if (token) {
-                document.getElementById('recaptcha').value = token;
-            }
-            });
-        });
-    </script>
 </head>
 <body>
     <div id="app">
