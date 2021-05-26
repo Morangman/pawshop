3f319540d3ed427a76986e43f6cee203bb09a9e6
Dmitry <sinepolsky.dmitry@gmail.com>
2021-05-26 21:44:49 +0300

    Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     @meta_tags
 
     <meta property="og:image:width" content="600">
