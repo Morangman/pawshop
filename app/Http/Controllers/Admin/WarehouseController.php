debc3bcb24d7ceaf07af0d287c9cf0c49417f8e7
Dmitry <sinepolsky.dmitry@gmail.com>
2021-06-02 20:36:52 +0300

    Xml upload

--- app/Http/Controllers/Admin/WarehouseController.php
+++ app/Http/Controllers/Admin/WarehouseController.php
@@ -294,14 +294,13 @@ class WarehouseController extends Controller
                     $price = $deliveryPrice / $products->count();
         
                     foreach ($products as $product) {
-                        if (!$product->getAttribute('delivery_price') || $product->getAttribute('delivery_price') === 0) {
-                            $devicePrice = (float) $product->getAttribute('clear_price') + $price;
+                        //if (!$product->getAttribute('delivery_price') || $product->getAttribute('delivery_price') === 0) {
+                            //$devicePrice = (float) $product->getAttribute('clear_price') + $price;
 
                             $product->update([
-                                'delivery_price' => $deliveryPrice,
-                                'clear_price' => (float) number_format($devicePrice, 2, '.', ''),
+                                'delivery_price' => (float) number_format($price, 2, '.', ''),
                             ]);
-                        }
+                        //}
                     }
                 }
             }
