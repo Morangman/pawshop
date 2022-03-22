72a4f6e974b757f248a7fd3f41a73f72fe6be96b
Morangman <sinepolsky.dmitry@gmail.com>
2022-03-22 22:00:00 +0200

    Check email new logic

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -71,7 +71,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(FedexLabel::class)->dailyAt('12:00');
         $schedule->command(SetCancelledOrder::class)->dailyAt('12:00');
         $schedule->command(CheckCardsAvailable::class)->everyMinute();
-        $schedule->command(CheckIphone::class)->everyMinute();
+        // $schedule->command(CheckIphone::class)->everyMinute();
         $schedule->command(SitemapGenerator::class)->daily();
         $schedule->command(ParseBuybackPrices::class)->daily();
     }
