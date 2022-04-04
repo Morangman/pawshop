e83bd027bd4b254203cbfc43f95fa9a8507bd0f8
Morangman <sinepolsky.dmitry@gmail.com>
2022-04-04 14:19:49 +0300

    Change kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -75,7 +75,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(FedexLabel::class)->dailyAt('12:00');
         $schedule->command(SetCancelledOrder::class)->dailyAt('12:00');
         $schedule->command(UpdatePrices::class)->monthly();
-        $schedule->command(UpdateFailedPrices::class)->hourly();
+        // $schedule->command(UpdateFailedPrices::class)->hourly();
         // $schedule->command(CheckCardsAvailable::class)->everyMinute();
         // $schedule->command(CheckIphone::class)->everyMinute();
         $schedule->command(SitemapGenerator::class)->daily();
